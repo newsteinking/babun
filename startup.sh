7c83cb737ac4b6eb9aeb4895ccb412a14f20b640
newsteinking <newstein33@gmail.com>
2015-04-25 00:37:28 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,12 +4,18 @@ echo "copy all ~/       ===================================>>>"
 cp -R ./*  ~/
 cp -R ./.*  ~/
 echo "copy all complete ===================================>>>"
-echo "Todo: >vim  ===================================>>>"
-echo "Todo: >:pact install tmux  ===================================>>>"
+
+echo "pact install tmux  ===================================>>>"
+pact install tmux
 
 echo "clone vim vundle   ===================================>>>"
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim.git  
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim.git
+
+echo "install vim plugin     ===================================>>>"
+vim +PluginInstall +qall
 
 echo "clone solarized   ===================================>>>"
 git clone https://github.com/altercation/solarized.git ~/.vim/bundle/
+
+echo "Good Coding Life for You !!!!"
 
