2107e1336101a1eccabd52b66335d6892ed31ac9
newstein33@gmail.com <newstein33@gmail.com>
2015-04-25 01:23:51 +0900

    dd

--- startup.sh
+++ startup.sh
@@ -9,13 +9,13 @@ echo "pact install tmux  ===================================>>>"
 pact install tmux
 
 echo "clone vim vundle   ===================================>>>"
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim.git
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "install vim plugin     ===================================>>>"
 vim +PluginInstall +qall
 
 echo "clone solarized   ===================================>>>"
-git clone https://github.com/altercation/solarized.git ~/.vim/bundle/
+git clone https://github.com/altercation/solarized.git ~/.vim/bundle/solarized
 
 echo "Good Coding Life for You !!!!"
 
