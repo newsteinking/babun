03a9e02c74c3f1f803d0764ecc9f385363a51cdb
sean <newstein33@gmail.com>
2015-04-27 10:03:48 +0900

    babun initial

--- startup.sh
+++ startup.sh
@@ -12,14 +12,18 @@ echo "copy all complete ===================================>>>"
 echo "pact install tmux  ===================================>>>"
 pact install tmux
 
+
+
+echo "clone solarized   ===================================>>>"
+git clone https://github.com/altercation/solarized.git ~/.vim/bundle/solarized
+
+
 echo "clone vim vundle   ===================================>>>"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "install vim plugin     ===================================>>>"
 vim +PluginInstall +qall
 
-echo "clone solarized   ===================================>>>"
-git clone https://github.com/altercation/solarized.git ~/.vim/bundle/solarized
 
 echo "Good Coding Life for You !!!!"
 
