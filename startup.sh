d9cd385da3c6912500f7cda78f85727e72e1c84e
sean <newstein33@gmail.com>
2015-04-27 10:27:28 +0900

    initial setup

--- startup.sh
+++ startup.sh
@@ -8,6 +8,8 @@ cp _vimrc ~/.vimrc
 cp _zshrc ~/.zshrc
 rm -rf ~/startup.sh
 
+
+
 echo "copy all complete ===================================>>>"
 
 echo "pact install tmux  ===================================>>>"
@@ -18,6 +20,8 @@ pact install tmux
 echo "clone solarized   ===================================>>>"
 git clone https://github.com/altercation/solarized.git ~/.vim/bundle/solarized
 
+echo "copy solarized.vim to colors dir   ===================================>>>"
+cp ~/.vim/bundle/solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
 
 echo "clone vim vundle   ===================================>>>"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
